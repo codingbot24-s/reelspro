1c6c4f6478c028602fd4239fcf3e22480b76c71a
Saad <primecoder8@gmail.com>
2025-02-01 16:12:05 +0530

    refactor database connection and improve environment loading

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,21 +4,35 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"context"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-
-
-func ConnectToDB() {
-	if err := godotenv.Load() ; err != nil {
-		log.Println("Error loading .env file")
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
+}
 
+func init () {
+	if err := connect_to_db(); err != nil {
+		log.Fatal("Error connecting to MongoDB")
+	}
+}
+
+func connect_to_db () *mongo.Client {
+	loadEnv()
 	uri := os.Getenv("MONGODB_URI")
-	fmt.Println(uri)
-
-	// connect to mongodb
-
-
+	if uri == "" {
+		log.Fatal("MONGODB_URI is not set")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
 }
